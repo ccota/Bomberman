ea16e734a128cd14b31a6a954e79a5d7dd94983d
Rodrigo Borba <rodrigo14.84@gmail.com>
2017-11-29 20:27:49 -0100

    rodrigo

--- src/bomberman/Factory.java
+++ src/bomberman/Factory.java
@@ -1,20 +1,25 @@
 package bomberman;
 
 import bomberman.Gameobjects.blocks.HardBlock;
+import bomberman.Gameobjects.blocks.SoftBlock;
 import bomberman.grid.Grid;
 
 public class Factory {
 
 
-
-
-
+    // To do later passar para enum blockos
+    /** Hard Block */
     public HardBlock hardBlocks(Grid grid,int x, int y){
 
         return new HardBlock(grid.makeGridPosition(x,y));
-
-
     }
+
+    /** Hard Block */
+    public SoftBlock softBlocks(Grid grid, int x, int y){
+        return new SoftBlock(grid.makeGridPosition(x,y));
+    }
+
+
 
 
 }
--- src/bomberman/Game.java
+++ src/bomberman/Game.java
@@ -1,25 +1,28 @@
 package bomberman;
 
-import bomberman.Gameobjects.blocks.HardBlock;
+import bomberman.Gameobjects.GameObjects;
 import bomberman.grid.Grid;
 import bomberman.grid.GridFactory;
 import bomberman.grid.GridType;
 
+import java.util.ArrayList;
+
 public class Game {
     private Grid grid;
     private Factory factory;
-    private HardBlock[] hardBlock;
+    private ArrayList<GameObjects> objects = new ArrayList<GameObjects>();
 
 
 
-    /*
+
+    /**
 	|--------------------------------------------------------------------------
 	| Game Options
 	|--------------------------------------------------------------------------
 	 */
     private GridType gridType= GridType.SIMPLE_GFX;
-    private int cols= 20;
-    private int rows=20;
+    private int cols= 15;
+    private int rows=15;
     private int delay=20;
 
 
@@ -27,39 +30,37 @@ public class Game {
     public Game (){
 
 
-
-
-
     }
 
-    /*
+
+
+
+    /**
 	|--------------------------------------------------------------------------
 	| Init/Create all the shit
 	|--------------------------------------------------------------------------
 	 */
     public void init(){
-        //Faz a grid
+
         grid = GridFactory.makeGrid(gridType, cols, rows);
 
-
-        //Criar HardBlocks
         factory = new Factory();
 
-
-
-
+        /* -------------| Hard Blocks |------------------ */
         for (int i = 1; i<grid.getCols(); i +=2){
             for (int j = 1 ; j < grid.getRows(); j += 2){
-                factory.hardBlocks(grid,i,j);
-                grid.makeGridPosition( i, j);
+                objects.add(factory.hardBlocks(grid,i,j));
             }
         }
+
+        //for ()
 
 
 
     }
 
     public void start(){
+        System.out.println(objects);
         init();
     }
 
