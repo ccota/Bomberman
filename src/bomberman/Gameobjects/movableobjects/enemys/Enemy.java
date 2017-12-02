d419aeb52f1498e81bdcd0b19b05d8f5b8b43ce9
CesarCota <cesarmiguelcota@gmail.com>
2017-12-02 18:07:30 -0100

    it's something

--- src/bomberman/Gameobjects/movableobjects/enemys/Enemy.java
+++ src/bomberman/Gameobjects/movableobjects/enemys/Enemy.java
@@ -23,24 +23,42 @@ abstract public class Enemy extends MovableObjects{
             case LEFT:
                 if (!collisionDetector.isUnSafe(getPos().getCol() -1, getPos().getRow())) {
                     getPos().moveInDirection(GridDirection.LEFT, 1);
+                    break;
                 }
+                currentDirection = GridDirection.RIGHT;
                 break;
             case RIGHT:
                 if (!collisionDetector.isUnSafe(getPos().getCol() +1, getPos().getRow())) {
                     getPos().moveInDirection(GridDirection.RIGHT, 1);
+                    break;
                 }
+
+                currentDirection = GridDirection.LEFT;
                 break;
             case UP:
                 if (!collisionDetector.isUnSafe(getPos().getCol() , getPos().getRow() -1)) {
                     getPos().moveInDirection(GridDirection.UP, 1);
+                    break;
                 }
+                currentDirection = GridDirection.DOWN;
                 break;
             case DOWN:
                 if (!collisionDetector.isUnSafe(getPos().getCol(), getPos().getRow()+1)) {
                     getPos().moveInDirection(GridDirection.DOWN, 1);
+                    break;
                 }
+                currentDirection = GridDirection.UP;
                 break;
+            default:
+                System.out.println("something went terribly wrong");
+
         }
+    }
+
+    private void chooseSafeDirection(GridDirection oldDirection){
+
+
+
     }
 
 
